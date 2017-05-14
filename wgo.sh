5ee22d418d900ad0a948034cec17f49463f279da
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-05-14 01:07:16 -0700

    Use absolute path to pre.js

--- wgo.sh
+++ wgo.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -ex
-EMMAKEN_CFLAGS="-s USE_SDL=2" cargo build --target asmjs-unknown-emscripten
-touch src/main.rs
-EMMAKEN_CFLAGS="-s USE_SDL=2 --pre-js pre.js" cargo build --target asmjs-unknown-emscripten
+EMMAKEN_CFLAGS="-s USE_SDL=2 --pre-js `pwd`/pre.js" cargo build --target asmjs-unknown-emscripten
 cp steven.html target/asmjs-unknown-emscripten/debug/
